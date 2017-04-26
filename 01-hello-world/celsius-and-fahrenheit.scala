0b2b3a735c9f022568f2718cf2dd06831af8a39d
MooYeol Prescott Lee <mooyoul@gmail.com>
2017-04-27 02:20:39 +0900

    add basic celsius and fahrenheit conversion

---
+++ 01-hello-world/celsius-and-fahrenheit.scala
@@ -0,0 +1,9 @@
+val celsius = 22.5
+println("Celsius: ", celsius)
+
+val fahrenheit = (celsius * 9/5) + 32
+println("fahrenheit: ", fahrenheit)
+
+val celsius2 = (fahrenheit - 32) * 5/9
+println("celsius2: ", celsius2)
+
